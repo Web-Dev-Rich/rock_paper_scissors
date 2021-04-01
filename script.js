a6b447118d49aaec407b66b8168a41f0abd7e85e
Rich Keyzor <web_dev_rich@keyzor.net>
2021-04-01 20:49:02 +0100

    Add repo link

--- script.js
+++ script.js
@@ -13,15 +13,32 @@ const playAgain = document.querySelector('.play-again');
 playAgain.classList.add('hidden');
 playAgain.removeEventListener('click', () => window.location.reload());
 
+// *** PLAY THE GAME WHEN A BUTTON IS CLICKED ***
 buttons.forEach(button => {
   button.addEventListener('click', handleClick);
 });
+
+function handleClick(e) {
+  const playerChoice = e.target.id;
+  const computerChoice = computerPlay();
+  const winner = playRound(playerChoice, computerChoice);
+
+  if (winner === 'p') {
+    playerTally += 1;
+  } else if (winner === 'c') {
+    computerTally += 1;
+  }
+  scoresDisp.textContent = `Your score: ${playerTally} Computer score: ${computerTally}`;
+
+  if (playerTally === 5 || computerTally === 5) {
+    declareResult(playerTally, computerTally);
+  }
+}
 
 // A `computerPlay` function creates the computers selection
 // **Parameters:** None
 // **Returns:** 'Rock', 'Paper' or 'Scissors'
 // **Variables changed:** None
-
 function computerPlay() {
   let output = '';
   const randomNumber = Math.floor(Math.random() * 3 + 1);
@@ -40,7 +57,6 @@ function computerPlay() {
 // **Returns:** true if a tie, 'p' if player wins, 'c' if computer wins
 // **Outputs:** A message to the console declaring the winner of the turn or if it was a tie
 // **Variables changed:** None
-
 function playRound(playerSelection, computerSelection) {
   choicesDisp.textContent = `You chose ${playerSelection}, Computer chose ${computerSelection}`;
 
@@ -68,6 +84,7 @@ function playRound(playerSelection, computerSelection) {
 // **Outputs:** A message to the console declaring the winner and scores of the game or if it was a tie
 // **Variables changed:** None
 function declareResult(playerScore, computerScore) {
+  // Remove the r,p & s event listeners so they don't pile up
   buttons.forEach(button => {
     button.removeEventListener('click', handleClick);
   });
@@ -79,23 +96,7 @@ function declareResult(playerScore, computerScore) {
   } else {
     winnerDisp.textContent = `Boo! You lose the game ${computerScore} to ${playerScore}`;
   }
+  // Show the play again button
   playAgain.classList.remove('hidden');
   playAgain.addEventListener('click', () => window.location.reload());
-}
-
-function handleClick(e) {
-  const playerChoice = e.target.id;
-  const computerChoice = computerPlay();
-  const winner = playRound(playerChoice, computerChoice);
-
-  if (winner === 'p') {
-    playerTally += 1;
-  } else if (winner === 'c') {
-    computerTally += 1;
-  }
-  scoresDisp.textContent = `Your score: ${playerTally} Computer score: ${computerTally}`;
-
-  if (playerTally === 5 || computerTally === 5) {
-    declareResult(playerTally, computerTally);
-  }
 }
